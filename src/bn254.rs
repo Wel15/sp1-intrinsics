d5dfd2087911198193da2d3e144bbbe4c3ccaf34
Wel15 <ffw150@163.com>
2025-01-17 01:12:42 -0500

    117fri

--- src/bn254.rs
+++ src/bn254.rs
@@ -1,7 +1,7 @@
 //! bn254 scalar operation
 
 /// `BN254_SCALAR_MUL` syscall ID.
-pub const BN254_SCALAR_MUL: u32 = 0x00_01_01_80;
+
 
 /// `BN254_SCALAR_MAC` syscall ID.
 pub const BN254_SCALAR_MAC: u32 = 0x00_01_01_81;
@@ -21,12 +21,12 @@ pub const BN254_SCALAR_MULADD: u32 = 0x00_01_01_1F;
 /// * `q` must be [valid] for reads of [`bn254::Fr`].
 ///
 /// * Both `p` and `q` must be properly aligned and not overlap.
-#[inline(always)]
-pub unsafe fn syscall_bn254_scalar_mul<P, Q>(p: *mut P, q: *const Q) {
-    unsafe {
-        crate::syscall!(BN254_SCALAR_MUL, p, q)
-    }
-}
+// #[inline(always)]
+// pub unsafe fn syscall_bn254_scalar_mul<P, Q>(p: *mut P, q: *const Q) {
+//     unsafe {
+//         crate::syscall!(BN254_SCALAR_MUL, p, q)
+//     }
+// }
 
 /// Perform in-place scalar multiplication and addition `ret += a + b`.
 ///
